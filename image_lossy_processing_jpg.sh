30634e6a871426cdd130098ac0245d6867abfa9d
shengshampoo <shengzh@tutanota.com>
2025-09-14 11:05:06 +0000

    Create image_lossy_processing_jpg.sh

---
+++ image_lossy_processing_jpg.sh
@@ -0,0 +1,15 @@
+
+#! /bin/bash
+
+set -e
+
+mkdir -p /work/artifact
+cd /images
+
+/usr/local/bin/rust-parallel -r '(.*)\.(.*)' -p /usr/local/bin/cjpeg -outfile {1}-fs2.jpg {0} ::: $(find /images -maxdepth 1 -name "*.jpg" ! -name "*fs2.jpg"  ! -name "*fs2a.jpg" -type f -exec basename {} \; | awk '!seen[$0]++' | awk '{ printf("%s " , $0) }')
+
+/usr/local/bin/rust-parallel -r '(.*)\.(.*)' -p /usr/local/bin/guetzli --verbose {0} {1}a.jpg ::: $(find /images -maxdepth 1 -name "*fs2.jpg" ! -name "*fs2a.jpg" -type f -exec basename {} \; | awk '!seen[$0]++' | awk '{ printf("%s " , $0) }')
+
+tar vcJf ./images.tar.xz *.jpg
+
+mv ./images.tar.xz /work/artifact/
